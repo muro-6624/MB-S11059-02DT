3f1b6f5e367ebeb9aba0ec131e89d5b3d3f627be
muro-6624 <murofushi.haruki@shizuoka.ac.jp>
2025-10-24 15:27:27 +0900

    初版

--- main.ts
+++ main.ts
@@ -1,1 +1,66 @@
+//% color="#A9A9A9" weight=100 icon="\uf75b"
+namespace カラーセンサー {
+    let IR = 0
+    let B = 0
+    let G = 0
+    let R = 0
 
+    //% block="初期化"
+    export function init() {
+        pins.i2cWriteNumber(
+            42,
+            139,
+            NumberFormat.UInt16BE,
+            false
+        )
+        pins.i2cWriteNumber(
+            42,
+            11,
+            NumberFormat.UInt16BE,
+            false
+        )
+    }
+
+    //% block="赤取得"
+    export function readR(): number{
+        pins.i2cWriteNumber(
+        42,
+        3,
+        NumberFormat.UInt8BE,
+        false
+        )
+        R = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        G = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        B = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        IR = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        return R;
+    }
+    //% block="緑取得"
+    export function readG(): number {
+        pins.i2cWriteNumber(
+            42,
+            3,
+            NumberFormat.UInt8BE,
+            false
+        )
+        R = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        G = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        B = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        IR = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        return G;
+    }
+    //% block="青取得"
+    export function readB(): number {
+        pins.i2cWriteNumber(
+            42,
+            3,
+            NumberFormat.UInt8BE,
+            false
+        )
+        R = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        G = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        B = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        IR = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        return B;
+    }
+}
